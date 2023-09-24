5710ca04bdcc71c94ad7a5d5a793a6dd6ad0675f
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-09-24 01:41:27 +0100

    added solution

--- Day 1/script.js
+++ Day 1/script.js
@@ -12,3 +12,29 @@ const ehc = (beneficiary) => {
 ehc()
 
 export default ehc;
+
+// CHALLENGE TWO
+/*
+- Initially, create an input field with the id 'nameInput' and a submit button with the id 'submitBtn'. You will add event listeners to both elements in JavaScript to achieve the following requirements:
+
+When the user clicks on the submit button without entering any text, display an error message with id 'errorMsg'.
+
+When the user starts typing in the input field after receiving the error message, the error message should be removed.
+
+Create a 'success' div with id 'successMsg', which will appear after successful validation. Success div should be initially hidden.
+
+Display the success message when the input field is not empty, and the submit button is clicked.
+
+Tasks
+Create an input field with id 'nameInput' and a button with id 'submitBtn'
+
+Implement a check to verify that the input name field is not empty when the submit button is clicked. Show an error message with id 'errorMsg' if the input field is empty.
+
+Remove the error message when the user types in the name input field after receiving the error message.
+
+Create a 'success' div with id 'successMsg', which will appear after successful validation. Success div should be initially hidden.
+
+Display the success message when the input field is not empty, and the submit button is clicked.
+*/
+let name = document.getElementById("nameInput");
+let btn = document.getElementById("submitBtn");

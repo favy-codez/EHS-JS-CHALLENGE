e33814706bd23e5fa28cc39380ebfe43a664c43a
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-09-26 14:25:37 +0100

    added day 3's challenge

---
+++ Day 3/script.js
@@ -0,0 +1,7 @@
+let countButton = document.getElementById("countBtn");
+let savedCount = document.getElementById("savedCount");
+let num = document.getElementById("number");
+
+countButton.addEventListener("click", function(){
+
+});
